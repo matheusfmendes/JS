f922c69aeb97ce3606c3f479feff6b54fba4816c
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2021-08-09 01:15:00 -0300

    001

---
+++ aula04/ex012.js
@@ -0,0 +1,10 @@
+let agora = new Date()
+let hora = agora.getDate()
+
+if (hora < 12) {
+	console.log("Bom dia")
+} else if (hora < 18) {
+	console.log("Boa tarde");
+} else {
+	console.log("Boa noite");
+}
