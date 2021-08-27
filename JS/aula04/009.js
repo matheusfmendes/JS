1098d13ad5a680e53280ffbeca62b5a13fcace0c
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2021-08-26 23:04:54 -0300

    Delete JS/aula04 directory

--- JS/aula04/009.js
+++
@@ -1,9 +0,0 @@
-let pais = 'EUA';
-
-console.log(`Vivendo no ${pais}`);
-
-if (pais == 'Brasil') {
-	console.log(`Brasileiro!`)
-} else {
-	console.log(`Estrangeiro!`)
-}
--- JS/aula04/012.js
+++
@@ -1,8 +0,0 @@
-let idade = 12
-if (idade < 16){
-	console.log('Nao vota')
-}else if (idade < 18 || idade > 65){
-console.log('voto opcional')
-}else{
-	console.log('Voto obrigatorio')
-}
