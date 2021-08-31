46ee6ef3be105cf32bb24ef0152087e6c725a9b5
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2021-08-30 23:47:10 -0300

    ajuste layout tabuada...

--- aula04/aula14ex/ex018/script.js
+++ aula04/aula14ex/ex018/script.js
@@ -2,29 +2,27 @@ function calcular() {
     let number = 0;
     let dividendo = document.getElementById('divi')
     let result = document.getElementById('res')
-    
+
     if (dividendo = Number <= 0) {
         dividendo = Number(divi.value.length)
-        
+
         for (number = -10; number <= 0; number++) {
             let resultado = dividendo * number;
-        
+
             result.innerHTML += `${dividendo} X ${number} = ${resultado} `
-            // console.log(`${dividendo} X ${number} = ${resultado}`);
-            // dividendo = Number(divi.value)
         }
     } else {
         result.innerHTML = "contando: "
-        for (number = 0; number <= 10; number++) {
-            dividendo = Number(divi.value)
-            result.style.overflowY = 'scroll'
-            result.style.width = '162px'
-            result.style.height = '250px'
-      
-            let resultado = dividendo * number;
-            result.innerHTML+= `<ul>${dividendo} X ${number} = ${resultado}</ul>`
-            // result.innerHTML+= `${dividendo} X ${number} = ${resultado}`
+        result.style.overflowY = 'scroll'
+        result.style.width = '162px'
+        result.style.height = '250px'
 
+        for (number = 0; number <= 10; number++) {
+
+            dividendo = Number(divi.value)
+
+            let resultado = dividendo * number;
+            result.innerHTML += `<ul>${dividendo} X ${number} = ${resultado}</ul>`
         }
     }
 
