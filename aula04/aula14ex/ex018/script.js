2863c253f3306855819bd9bd7344724c204c4a02
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2021-09-01 11:42:46 -0300

    Update script.js

--- aula04/aula14ex/ex018/script.js
+++ aula04/aula14ex/ex018/script.js
@@ -4,25 +4,19 @@ function calcular() {
     let result = document.getElementById('res')
 
     if (dividendo = Number <= 0) {
+        
         dividendo = Number(divi.value.length)
-
-        for (number = -10; number <= 0; number++) {
-            let resultado = dividendo * number;
-
-            result.innerHTML += `${dividendo} X ${number} = ${resultado} `
-        }
-    } else {
+        
         result.innerHTML = "contando: "
-        // result.style.overflowY = 'scroll'
-        // result.style.width = '162px'
-        // result.style.height = '250px'
-
-        for (number = 0; number <= 10; number++) {
-
-            dividendo = Number(divi.value)
+        
+             for (number = 0; number <= 10; number++) {
 
             let resultado = dividendo * number;
             result.innerHTML += `<option>${dividendo} X ${number} = ${resultado}</option>`
+            
+        // result.style.overflowY = 'scroll'
+        // result.style.width = '162px'
+        // result.style.height = '250px'
         }
     }
 
